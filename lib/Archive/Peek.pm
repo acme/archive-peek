e232133cdd8b7f84020f5a11258ab728940822f1
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-11-12 21:40:40 -0500

    Prep for release v0.36

--- lib/Archive/Peek.pm
+++ lib/Archive/Peek.pm
@@ -4,7 +4,7 @@ use Carp qw(confess);
 use Archive::Peek::Tar;
 use Archive::Peek::Zip;
 use Types::Path::Tiny qw( File );
-our $VERSION = '0.35';
+our $VERSION = '0.36';
 
 has 'filename' => (
     is       => 'ro',
